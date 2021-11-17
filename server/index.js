44871a549ce5a25f5fe5e16f560c65bd17f824fe
zugzw4ng <bongcloud2001@gmail.com>
2021-11-17 12:49:11 -0500

    more initial server setup

--- server/index.js
+++ server/index.js
@@ -1,78 +1,32 @@
-const express = require('express')
-const session = require('express-session')
-const crypto = require('crypto')
-const fetch = require("node-fetch");
+const express = require('express');
+const session = require('express-session');
+const passport = require('passport');
+const LichessStrategy = require('./lib').Strategy;
 
-const app = express()
-const port = 3000
-const clientId = 'example-backend'
+passport.use(new LichessStrategy({
+  clientID: 'example.com',
+  callbackURL: 'http://localhost:5000/auth/lichess/callback'
+}, (_accessToken, _refreshToken, profile, cb) => cb(null, profile)));
 
-app.use(session({ resave: true, secret: 'SECRET', saveUninitialized: true }));
+passport.serializeUser((user, cb) => cb(null, user));
+passport.deserializeUser((obj, cb) => cb(null, obj));
 
-app.get('/', (req, res) => {
-  res.send('<a href="/login">Login</a>')
-})
+const app = express();
 
-// LOGIN
-const base64URLEncode = (str) => {
-  return str.toString('base64')
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_')
-    .replace(/=/g, '');
-}
+app.use(session({secret:'some-secret', resave: true, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
 
-const sha256 = (buffer) => crypto.createHash('sha256').update(buffer).digest();
+app.get('/auth/lichess', passport.authenticate('lichess'));
 
-const createVerifier = () => base64URLEncode(crypto.randomBytes(32));
+app.get('/auth/lichess/callback',
+  passport.authenticate('lichess', {
+    successRedirect: '/ok',
+    failureRedirect: '/nope'
+  }));
 
-const createChallenge = (verifier) => base64URLEncode(sha256(verifier));
+app.get('/', (_req, res) => res.send('<a href="http://localhost:5000/auth/lichess">Login</a>'));
+app.get('/ok', (req, res) => res.send(`Hello ${req.user.username}!`));
+app.get('/nope', (_req, res) => res.send('Authentication failed'));
 
-app.get('/login', async (req, res) => {
-  const url = req.protocol + '://' + req.get('host') + req.baseUrl;
-  const verifier = createVerifier()
-  const challenge = createChallenge(verifier)
-  req.session.codeVerifier = verifier
-  res.redirect('https://lichess.org/oauth?' + new URLSearchParams({
-    response_type: 'code',
-    client_id: clientId,
-    redirect_uri: `${url}/callback`,
-    scope: 'preference:read',
-    code_challenge_method: 'S256',
-    code_challenge: challenge
-  }))
-})
-
-// CALLBACK
-const getLichessToken = async (authCode, verifier, url) => await fetch('https://lichess.org/api/token', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({
-    grant_type: 'authorization_code',
-    redirect_uri: `${url}/callback`,
-    client_id: clientId,
-    code: authCode,
-    code_verifier: verifier,
-  })
-}).then(res => res.json());
-
-const getLichessUser = async (accessToken) => await fetch('https://lichess.org/api/account', {
-  headers: {
-    'Authorization': `Bearer ${accessToken}`
-  }
-}).then(res => res.json());
-
-app.get('/callback', async (req, res) => {
-  const url = req.protocol + '://' + req.get('host') + req.baseUrl;
-  const verifier = req.session.codeVerifier;
-  const lichessToken = await getLichessToken(req.query.code, verifier, url)
-
-  if (!lichessToken.access_token) {
-    res.send('Failed getting token');
-    return
-  }
-
-  const lichessUser = await getLichessUser(lichessToken.access_token)
-  res.send(`Logged in as ${lichessUser.username}`)
-})
-
-app.listen(port)
+app.listen(5000, () => console.log('http://localhost:5000/'));
