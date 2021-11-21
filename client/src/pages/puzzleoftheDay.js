ba32948e232fe65758c98199d5f35f653679924a
curriecoder <andrew.j.currie0@gmail.com>
2021-11-21 18:05:58 -0500

    preparing to merge

--- client/src/pages/puzzleoftheDay.js
+++ client/src/pages/puzzleoftheDay.js
@@ -1,18 +1,28 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
-const  PuzzleDay = () => {
-
-
-    return (
-      <div style={{ background: "#2b313c", height: "100vh" }}>
-      <div className="test">
-      </div>
+const PuzzleDay = () => {
+  return (
+    <div style={{ background: "#2b313c", height: "100vh" }}>
+      <div className="test"></div>
       <div>
-        <iframe src="https://lichess.org/training/frame?theme=brown&bg=dark" target="_blank" rel = "noopener noreferrer" style={{width: 400, height: 444, allowtransparency:"true", frameborder:"0"}} title='dailyPuzzle'></iframe>
-          </div>
+        <iframe
+          src="https://lichess.org/training/frame?theme=brown&bg=dark"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={{
+            width: 400,
+            height: 444,
+            allowtransparency: "true",
+            frameborder: "0",
+          }}
+          title="dailyPuzzle"
+        ></iframe>
+        <br/>
+        <a href="https://lichess.org/training">Click here for daily puzzle</a>
       </div>
-    );
-  }
+    </div>
+  );
+};
 
-  export default PuzzleDay;
+export default PuzzleDay;
